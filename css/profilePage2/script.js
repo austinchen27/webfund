66f6df243d70e63a6a77de2ba469678c0d6f311e
Austin <austinchen27@gmail.com>
2022-10-18 16:46:59 -1000

    update: making connections (w/out bonus)

---
+++ css/profilePage2/script.js
@@ -0,0 +1,19 @@
+// === USER CARD ===
+function newName() {
+  var nameOne = document.querySelector("#title");
+  nameOne.innerText = "GOD OF PIZZA";
+}
+
+function hide(element) {
+  element.remove();
+}
+
+
+// ======= YOUR CONNECTIONS =======
+
+// var count = [2]
+
+// function add1() {
+//   var countElement = document.querySelector("#co-no-");
+//   count-Element.innerText = "+(count++)"
+// }
